7043985779faf9eb23d7297b487ebbb182cf65f9
roman <roman@DESKTOP-7J5CLLN>
2023-03-06 04:41:31 +0300

    Добавьте файлы проекта.

---
+++ Replace/Replace.cpp
@@ -0,0 +1,71 @@
+﻿#include <cassert>
+#include <fstream>
+#include <iostream>
+#include <string>
+
+class StreamReplacer
+{
+public:
+	StreamReplacer() {}
+	StreamReplacer(const std::string& pattern, const std::string& replacement)
+		: m_pattern(pattern)
+		, m_replacement(replacement)
+	{
+	}
+
+	void replaceInFile(const std::string& fInPath, const std::string fOutPath)
+	{
+		std::ifstream input(fInPath, std::ios::in);
+		std::ofstream output(fOutPath, std::ios::out);
+
+		assert(input.is_open());
+		assert(output.is_open());
+
+		std::string readData;
+		while (std::getline(input, readData))
+		{
+			output << replace(readData) << std::endl;
+		}
+
+		input.close();
+		output.close();
+	}
+
+private:
+	std::string m_pattern;
+	std::string m_replacement;
+
+	std::string replace(const std::string& line)
+	{
+		std::string result = "";
+
+		size_t offset = 0;
+		size_t currPos = 0;
+		while (true)
+		{
+			offset = line.find(m_pattern, currPos);
+			if (offset == std::string::npos)
+				break;
+
+			result += line.substr(currPos, offset - currPos);
+			result += m_replacement;
+
+			currPos += offset - currPos + m_pattern.size();
+		}
+		result += line.substr(currPos, line.size() - currPos);
+		return result;
+	}
+};
+
+int main(int argc, char** argv)
+{
+	assert(argc == 5);
+
+	std::string inputFileName(argv[1]),
+		outputFileName(argv[2]),
+		searchString(argv[3]),
+		replacementString(argv[4]);
+
+	StreamReplacer replacer(searchString, replacementString);
+	replacer.replaceInFile(inputFileName, outputFileName);
+}
