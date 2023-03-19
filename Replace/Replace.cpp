aaf9df31c7e6fe27c60afab0469972ca1d268632
roman <karmazanovr@mail.ru>
2023-03-20 01:27:57 +0300

    Добавил тесты, поправил отступы, добавил валидацию для searchString

--- Replace/Replace.cpp
+++ Replace/Replace.cpp
@@ -6,66 +6,84 @@
 class StreamReplacer
 {
 public:
-	StreamReplacer() {}
-	StreamReplacer(const std::string& pattern, const std::string& replacement)
-		: m_pattern(pattern)
-		, m_replacement(replacement)
-	{
-	}
+    StreamReplacer() {}
+    StreamReplacer(const std::string& pattern, const std::string& replacement)
+        : m_pattern(pattern)
+        , m_replacement(replacement)
+    {
+        Validate(); 
+    }
 
-	void ReplaceInFile(const std::string& fInPath, const std::string fOutPath)
-	{
-		std::ifstream input(fInPath, std::ios::in);
-		std::ofstream output(fOutPath, std::ios::out);
+    void ReplaceInFile(const std::string& fInPath, const std::string fOutPath)
+    {
+        OpenStreams(fInPath, fOutPath);
 
-		assert(input.is_open());
-		assert(output.is_open());
+        std::string readData;
+        while (std::getline(m_input, readData))
+        {
+            m_output << Replace(readData) << std::endl;
+        }
 
-		std::string readData;
-		while (std::getline(input, readData))
-		{
-			output << Replace(readData) << std::endl;
-		}
-
-		input.close();
-		output.close();
-	}
+        CloseStreams();
+    }
 
 private:
-	std::string m_pattern;
-	std::string m_replacement;
+    std::string m_pattern;
+    std::string m_replacement;
+    std::ifstream m_input;
+    std::ofstream m_output;
 
-	std::string Replace(const std::string& line)
-	{
-		std::string result = "";
+    void Validate()
+    {
+        assert(!m_pattern.empty());
+    }
 
-		size_t offset = 0;
-		size_t currPos = 0;
-		while (true)
-		{
-			offset = line.find(m_pattern, currPos);
-			if (offset == std::string::npos)
-				break;
+    std::string Replace(const std::string& line)
+    {
+        std::string result = "";
 
-			result += line.substr(currPos, offset - currPos);
-			result += m_replacement;
+        size_t offset = 0;
+        size_t currPos = 0;
+        while (true)
+        {
+            offset = line.find(m_pattern, currPos);
+            if (offset == std::string::npos)
+                break;
 
-			currPos += offset - currPos + m_pattern.size();
-		}
-		result += line.substr(currPos, line.size() - currPos);
-		return result;
-	}
+            result += line.substr(currPos, offset - currPos);
+            result += m_replacement;
+
+            currPos += offset - currPos + m_pattern.size();
+        }
+        result += line.substr(currPos, line.size() - currPos);
+        return result;  
+    }
+
+    void OpenStreams(const std::string& fInPath, const std::string fOutPath)
+    {
+        m_input.open(fInPath, std::ios::in);
+        m_output.open(fOutPath, std::ios::out);
+
+        assert(m_input.is_open());
+        assert(m_output.is_open());
+    }
+
+    void CloseStreams()
+    {
+        m_input.close();
+        m_output.close();
+    }
 };
 
 int main(int argc, char** argv)
 {
-	assert(argc == 5);
+    assert(argc == 5);
 
-	std::string inputFileName(argv[1]),
-		outputFileName(argv[2]),
-		searchString(argv[3]),
-		replacementString(argv[4]);
+    std::string inputFileName(argv[1]),
+        outputFileName(argv[2]),
+        searchString(argv[3]),
+        replacementString(argv[4]);
 
-	StreamReplacer replacer(searchString, replacementString);
-	replacer.ReplaceInFile(inputFileName, outputFileName);
+    StreamReplacer replacer(searchString, replacementString);
+    replacer.ReplaceInFile(inputFileName, outputFileName);
 }
---
+++ Replace/tester.bat
@@ -0,0 +1,35 @@
+set PROGRAM="%~1"
+set TEST_LOC="tests"
+set TEST_TGT="test_target_results"
+
+%PROGRAM% "%TEST_LOC%\all_replaced.txt" "%TEMP%\all_replaced_output.txt" abc replaced  goto err
+echo TEST #1 (all_replaced) ... 
+fc.exe "%TEMP%\all_replaced_output.txt" "%TEST_TGT%\all_replaced_needed.txt"  goto err
+echo TEST #1 PASSED
+
+%PROGRAM% "%TEST_LOC%\empty_input.txt" "%TEMP%\empty_input_output.txt" some_text_1 some_text_2  goto err
+echo TEST #2 (empty_input) ...
+fc.exe "%TEMP%\empty_input_output.txt" "%TEST_TGT%\empty_input_needed.txt"  goto err
+echo TEST #2 PASSED
+
+%PROGRAM% "%TEST_LOC%\no_replacements.txt" "%TEMP%\no_replacements_output.txt" xx why_it_replaced  goto err
+echo TEST #3 (no_replacements) ...
+fc.exe "%TEMP%\no_replacements_output.txt" "%TEST_TGT%\no_replacements_needed.txt"  goto err
+echo TEST #3 PASSED
+
+%PROGRAM% "%TEST_LOC%\overlap.txt" "%TEMP%\overlap_output.txt" 1234 12345 goto err
+echo TEST #4 (overlap) ...
+fc.exe "%TEMP%\overlap_output.txt" "%TEST_TGT%\overlap_output_needed.txt"  goto err
+echo TEST #4 PASSED
+
+echo TEST #5 (invalid_search_pattern) ...
+%PROGRAM% "%TEST_LOC%\large_input.txt" "%TEMP%\invalid_search_pattern_output.txt" "" heyy_dude goto okay
+echo TEST #5 PASSED
+
+:okay
+echo OK
+exit 0
+
+:err
+echo Program testing failed
+exit 1
