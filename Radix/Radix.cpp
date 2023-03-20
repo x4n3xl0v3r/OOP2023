9485b0917dbed8262efa02d6502b8a961dc9b550
roman <karmazanovr@mail.ru>
2023-03-20 03:11:11 +0300

    Поправил тесты в Replace и добавил тесты для Radix; поправил Radix

--- Radix/Radix.cpp
+++ Radix/Radix.cpp
@@ -4,152 +4,194 @@
 
 enum Errcodes
 {
-	ERR_OVERFLOW = 1,
-	ERR_INVALID_CHAR = 2,
-	ERR_INVALID_RADIX = 3
+    ERR_OVERFLOW = 1,
+    ERR_INVALID_CHAR = 2,
+    ERR_INVALID_RADIX = 3
 };
 
 template <class Signed>
 bool isMultiplyOverflowSigned(Signed a, Signed b)
 {
-	Signed c = 9223372036854775807;
-	if ((b != 0) && (a > (c / b)))
-		return true;
-	return false;
+    Signed c = 9223372036854775807; // Decimal 64bit signed int representation
+    if ((b != 0) && (a > (c / b)))
+        return true;
+    return false;
 }
 
 template <class Signed>
 bool isSumOverflowSigned(Signed a, Signed b)
 {
-	Signed c = 9223372036854775807;
-	if (a > (c - b))
-		return true;
-	return false;
+    Signed c = 9223372036854775807;
+    if (a > (c - b))
+        return true;
+    return false;
 }
 
-bool SkipMinus(const std::string& str)
+bool IsNegative(const std::string& str)
 {
-	if (str.front() == '-')
-	{
-		return true;
-	}
-	return false;
+    if (str.front() == '-')
+    {
+        return true;
+    }
+    return false;
+}
+
+template<class Signed>
+Signed GetDigitalRepr(char digit, bool &err)
+{
+    int alphaStartCode = 'A';
+    int digitStartCode = '0';
+    err = false;
+
+    if (isalpha(digit))
+    {
+        return static_cast<Signed>(toupper(digit) - alphaStartCode + 10);
+    }
+    else if (isdigit(digit))
+    {
+        return static_cast<Signed>(digit - digitStartCode);
+    }
+    else
+    {
+        return -1;
+        err = true;
+    }
 }
 
 long long StringToLLInt(const std::string& str, int radix, bool& wasError)
 {
-	if ((radix < 2) || (radix > 36))
-	{
-		wasError = true;
-		return Errcodes::ERR_INVALID_RADIX;
-	}
+    if ((radix < 2) || (radix > 36))
+    {
+        wasError = true;
+        return Errcodes::ERR_INVALID_RADIX;
+    }
 
-	bool isNegative = SkipMinus(str);
+    bool isNegative = IsNegative(str);
 
-	int alphaStartCode = 'A';
-	int digitStartCode = '0';
-	long long wideRadix = radix;
+    int alphaStartCode = 'A';
+    int digitStartCode = '0';
+    long long wideRadix = radix;
 
-	long long value = 0;
-	long long base = 1;
-	long long addPart;
+    long long value = 0;
+    long long base = 1;
+    long long addPart;
 
-	size_t negativeOffset = static_cast<size_t>(isNegative);
-	std::string positiveString = str.substr(negativeOffset, str.size() - negativeOffset);
+    size_t negativeOffset = static_cast<size_t>(isNegative);
+    std::string positiveString = str.substr(negativeOffset, str.size() - negativeOffset);
 
-	long long currNum;
-	for (auto strIter = positiveString.rbegin(); strIter != positiveString.rend(); ++strIter)
-	{
-		if (isalpha(*strIter))
-		{
-			currNum = static_cast<long long>(toupper(*strIter)) - alphaStartCode + 10;
-		}
-		else if (isdigit(*strIter))
-		{
-			currNum = static_cast<long long>(*strIter) - digitStartCode;
-		}
-		else
-		{
-			wasError = true;
-			return Errcodes::ERR_INVALID_CHAR;
-		}
+    long long currNum;
+    bool isErrored;
+    for (auto strIter = positiveString.rbegin(); strIter != positiveString.rend(); ++strIter)
+    {
+        isErrored = false;
+        currNum = GetDigitalRepr<long long>(*strIter, isErrored);
 
-		if (isMultiplyOverflowSigned(base, currNum))
-		{
-			wasError = true;
-			return Errcodes::ERR_OVERFLOW;
-		}
-		addPart = base * currNum;
+        if (isErrored || (currNum >= radix))
+        {
+            wasError = true;
+            return Errcodes::ERR_INVALID_CHAR;
+        }
 
-		if (isSumOverflowSigned(value, addPart))
-		{
-			wasError = true;
-			return Errcodes::ERR_OVERFLOW;
-		}
+        if (isMultiplyOverflowSigned(base, currNum))
+        {
+            wasError = true;
+            return Errcodes::ERR_OVERFLOW;
+        }
+        addPart = base * currNum;
 
-		value += addPart;
-		base *= wideRadix;
-	}
-	return value;
+        if (isSumOverflowSigned(value, addPart))
+        {
+            wasError = true;
+            return Errcodes::ERR_OVERFLOW;
+        }
+
+        value += addPart;
+        base *= wideRadix;
+    }
+
+    if (isNegative)
+    {
+        value *= -1;
+    }
+
+    return value;
 }
 
 std::string IntToString(long long n, int radix, bool& wasError)
 {
-	if ((radix < 2) || (radix > 36))
-	{
-		wasError = true;
-		return "Invalid destination radix";
-	}
+    if ((radix < 2) || (radix > 36))
+    {
+        wasError = true;
+        return "Invalid destination radix";
+    }
 
-	constexpr char alphabet[] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	std::string result = "";
-	while (n != 0)
-	{
-		size_t letterPos = abs(n % radix);
-		n /= radix;
-		result = alphabet[letterPos] + result;
-	}
-	return result;
+    constexpr char alphabet[] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    std::string result = "";
+
+    bool isNegativeSign = false;
+    if (n < 0)
+    {
+		isNegativeSign = true;
+    }
+
+    while (n != 0)
+    {
+        size_t letterPos = abs(n % radix);
+        n /= radix;
+        result = alphabet[letterPos] + result;
+    }
+
+    if (isNegativeSign)
+    {
+        result = '-' + result;
+    }
+
+    return result;
 }
 
 long long ParseLL(const char* s)
 {
-	char* endp;
-	long long result = _strtoi64(s, &endp, 10);
-	return result;
+    char* endp;
+    long long result = _strtoi64(s, &endp, 10);
+    return result;
 }
 
 int main(int argc, char** argv)
 {
-	assert(argc == 4);
+    assert(argc == 4);
 
-	int srcNotation = static_cast<int>(ParseLL(argv[1]));
-	int dstNotation = static_cast<int>(ParseLL(argv[2]));
+    int srcNotation = static_cast<int>(ParseLL(argv[1]));
+    int dstNotation = static_cast<int>(ParseLL(argv[2]));
 
-	std::string value(argv[3]);
+    std::string value(argv[3]);
 
-	bool wasError = false;
-	auto decimalValue = StringToLLInt(value, srcNotation, wasError);
+    bool wasError = false;
+    auto decimalValue = StringToLLInt(value, srcNotation, wasError);
 
-	if (wasError)
-	{
-		switch (decimalValue)
-		{
-		case Errcodes::ERR_INVALID_CHAR:
-			std::cout << "Invalid input value\n";
-			break;
-		case Errcodes::ERR_INVALID_RADIX:
-			std::cout << "Invalid source radix\n";
-			break;
-		case Errcodes::ERR_OVERFLOW:
-			std::cout << "Overflow error\n";
-			break;
-		default:
-			std::cout << "Undefined error\n";
-		}
-		return 0;
-	}
+    if (wasError)
+    {
+        switch (decimalValue)
+        {
+        case Errcodes::ERR_INVALID_CHAR:
+            std::cout << "Invalid input value\n";
+            break;
+        case Errcodes::ERR_INVALID_RADIX:
+        std::cout << "Invalid source radix\n";
+            break;
+        case Errcodes::ERR_OVERFLOW:
+            std::cout << "Overflow error\n";
+            break;
+        default:
+            std::cout << "Undefined error\n";
+        }
+        return -1;
+    }
 
-	auto outputString = IntToString(decimalValue, dstNotation, wasError);
-	std::cout << outputString << '\n';
+    auto outputString = IntToString(decimalValue, dstNotation, wasError);
+    std::cout << outputString << '\n';
+    if (wasError)
+    {
+        return -1;
+    }
+    return 0;
 }
---
+++ Radix/tester.bat
@@ -0,0 +1,31 @@
+@echo off
+set PROGRAM="%~1"
+set TEST_TGT="test_target_results"
+
+echo TEST #1 (digit_out_of_range)
+%PROGRAM% 10 2 ABCD > "%TEMP%\digit_out_of_range.txt"
+fc "%TEMP%\digit_out_of_range.txt" "%TEST_TGT%\digit_out_of_range.txt"  goto err
+
+echo TEST #2 (same_radix_pass)
+%PROGRAM% 10 10 532 > "%TEMP%\same_radix_pass.txt"
+fc "%TEMP%\same_radix_pass.txt" "%TEST_TGT%\same_radix_pass.txt"  goto err
+
+echo TEST #3 (out_of_range)
+%PROGRAM% 16 10 DEADBEEFDEADBEEF > "%TEMP%\out_of_range.txt"
+fc "%TEMP%\out_of_range.txt" "%TEST_TGT%\out_of_range.txt"  goto err
+
+echo TEST #4 (invalid_src_radix)
+%PROGRAM% 105 5 1337228 > "%TEMP%\invalid_src_radix.txt"
+fc "%TEMP%\invalid_src_radix.txt" "%TEST_TGT%\invalid_src_radix.txt"  goto err
+
+echo TEST #5 (invalid_src_radix)
+%PROGRAM% 16 666 1337228 > "%TEMP%\invalid_dst_radix.txt"
+fc "%TEMP%\invalid_dst_radix.txt" "%TEST_TGT%\invalid_dst_radix.txt"  goto err
+
+echo TEST #6 (invalid_char)
+%PROGRAM% 16 8 "!153" > "%TEMP%\invalid_char.txt"
+fc "%TEMP%\invalid_char.txt" "%TEST_TGT%\invalid_char.txt"  goto err
+
+echo TEST #7 (signed_processing)
+%PROGRAM% 10 16 -64 > "%TEMP%\signed_processing.txt"
+fc "%TEMP%\signed_processing.txt" "%TEST_TGT%\signed_processing.txt"  goto err
